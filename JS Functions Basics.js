ad99a7fe5332d2c385e4a09f7fb925f7b5cec3c9
eldhomathew9076 <eldhomathew9076@gmail.com>
2026-07-10 09:32:30 +0530

    Add files via upload

---
+++ JS Functions Basics.js
@@ -0,0 +1,21 @@
+// 1
+function IamEldho() {
+  return "I am Eldho";
+}
+
+let name = IamEldho();
+console.log(name);
+
+// 2
+function JSTrials(){
+    return "JavaScript";
+}
+let text =JSTrials();
+console.log(text);
+
+//3
+function Data() {
+    return "Information is Wealth";
+}
+let quote =Data();
+console.log(quote);
