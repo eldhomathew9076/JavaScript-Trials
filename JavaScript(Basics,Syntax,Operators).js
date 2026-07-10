815e553845c54e54aa6e35c98a4d2f22456f8219
eldhomathew9076 <eldhomathew9076@gmail.com>
2026-07-10 08:46:20 +0530

    Add files via upload

---
+++ JavaScript(Basics,Syntax,Operators).js
@@ -0,0 +1,240 @@
+let x = 5;
+let y = 4;
+let z = 6;
+let a = (x + y + z);
+console.log(a);
+ 
+let b=10;
+let c=5;
+let d=15;
+let e=(b*c*d);
+console.log(e);
+
+let f=20;
+let g=30;
+let h =(g-f);
+console.log(h);
+
+let a1=100;
+let a2=200;
+let a3=(a2/a1);
+console.log(a3);
+
+let a4=50;
+let a5=(a4**2);
+console.log(a5);
+
+let x1;
+x1=7;
+console.log(x1);
+
+let x2 =3;
+let x3=(200+50)*x2;
+console.log(x3);
+
+let text1="Eldho";
+let text2="Mathew";
+let text3=(text1+" "+text2);
+console.log(text3);
+
+let text4="What is a ";
+text4 += "Computer ?";
+console.log(text4);
+
+let x5 =10;
+let x6 =5;
+let x7 =(x5%x6);
+console.log(x7);    
+
+let x8 = 10;
+let x9 = 5;
+let x10 = (x8==x9);
+console.log(x10);
+
+let x11=10; 
+x11 +=5;
+console.log(x11);
+
+let y1 =5;
+let result = y1>8;
+console.log(result);
+
+let y2=10;
+let result1 =y2<25;
+console.log(result1);
+
+const pi =3.14;
+let r=5;
+let area=pi*r**2;
+console.log(area);
+
+let volume =4/3*pi*r**3;
+console.log(volume);
+
+let spherearea=4*pi*r**2;
+console.log(spherearea); 
+
+let H =10;
+let cylindervolume =pi*H*r**2;
+console.log(cylindervolume);
+
+const cars =["audi","bently","chevy","daimler"];
+ console.log(cars);
+
+ cars.push("eicher");
+    console.log(cars[0]);
+    cars[0]="aston martin";
+    console.log(cars);
+const car ={type:"Fiat",model:"500",color:"White"};
+console.log(car);
+
+typeof 3.14
+typeof 314
+typeof "a"
+typeof "true"
+typeof "Eldho"
+console.log(typeof 3.14);
+console.log(typeof 314);
+console.log(typeof "a");
+console.log(typeof "true");
+console.log(typeof "Eldho");
+
+let X=5;
+(X == 8);
+(X != 8);
+console.log(X == 8);
+console.log(X != 8);
+
+let X1=10;
+let X2=4;
+let X3=(X1%X2);
+console.log(X3);
+
+let X4=17000567;
+let X5=23001;
+let X6=(X4%X5);
+console.log(X6);
+
+let E= 5;
+E++;
+let F=E;
+console.log(F);
+
+let G=10;
+G--;
+let H1=G;
+console.log(H1);
+
+let X22 =5;
+let Z1 = Math.pow(X22,2);
+console.log(Z1);
+
+let x21 = 10;
+x21 += 5;
+console.log(x21);
+
+let x23=10;
+let y23= 5;
+let z23=x23+y23;
+console.log(z23);
+
+let x24=10;
+let y24=5;
+let z24=x24-y24;
+console.log(z24);
+
+let x25=10;
+x25-=5;
+console.log(x25);
+
+let x26=100;
+let y26=5;
+let z26=x26*y26;
+console.log(z26);
+
+let x27=100;
+x27 *=5;
+console.log(x27);
+
+let x28=3;
+x28 **=3;
+console.log(x28);
+
+let x29=4;
+x29 **=2;
+console.log(x29);
+
+let x30=10;
+x30 /=5;
+console.log(x30);
+
+let x31=10;
+x31 /=2;
+console.log(x31);
+
+let x32=10;
+x32 %=3;
+console.log(x32);
+
+let text5="Hello ";
+text5+="Master";
+console.log(text5);
+
+let q = false;
+let p = q &&= 10;
+console.log(q);
+
+let q1 = true;
+let p1 = x &&= 10;
+console.log(q1);
+
+let q2 = 1;
+let p2 = x &&= 10;
+console.log(q2);
+
+let q3 = 0;
+let p3 = x &&= 10;
+console.log(q3);
+
+let q5 = null;
+let p5 = x &&= 10;
+console.log(q5);
+
+let q6 = false;
+let p6 = q6 ||= 10;
+console.log(q6);
+
+let q7 = true;
+let p7 = q7 ||= 10;
+console.log(q7);
+
+let q8 = null;
+let p8 = q8 ||= 10;
+console.log(q8);
+
+let q9 = null;
+let p9 = q9 ||=8;
+console.log(q9);
+
+let j;
+j ??= 10;
+console.log(j)
+
+let j1=0;
+j1 ??= 10;
+console.log(j1);
+
+let j2=null;
+j2 ??= 10;
+console.log(j2);
+
+let j3=10;
+let j4=5;
+j3??=10;
+console.log(j3);
+
+
+
+
+
+    
